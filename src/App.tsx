ab2de75c124a874ff83ce43a77d54a7fa133250f
talel <talelmejri8@gmail.com>
2025-11-22 17:51:11 +0100

    fiw bugs

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,14 @@ import TestimonialsSection from "./components/layouts/TestimonialsSection";
 import AwardsSection from "./components/layouts/AwardsSection";
 import ProjectsSection from "./components/layouts/ProjectsSection";
 import SocialMediaSection from "./components/layouts/SocialMedia";
-import simpleNotificationManager from '@/services/simpleNotificationManager';
 import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    simpleNotificationManager.initialize();
-    setTimeout(() => {
-      simpleNotificationManager.sendCustomNotification("Test Notification", "This is a test notification body.");
-    }, 5000);
+    // simpleNotificationManager.initialize();
+    // setTimeout(() => {
+    //   simpleNotificationManager.sendCustomNotification("Test Notification", "This is a test notification body.");
+    // }, 5000);
 
   }, []);
   return (
